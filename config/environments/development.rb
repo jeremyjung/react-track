b0186b0d41f78e06caa41ab81eeec0f296b65cec
Jeremy Jung <jeremyjung@users.noreply.github.com>
2017-06-25 14:55:31 -0700

    Get rid of CoffeeScript and asset generators.  Add Procfile to start dev-server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Don't create assets when using generators
+  config.generators.assets = false
 end
