04a1e77cc0574ca4f2a9f73e3b0d2258a874d34a
Jeremy Jung <jeremyjung@users.noreply.github.com>
2017-06-25 17:13:31 -0700

    Add a nav bar and an about and talks page

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -7,7 +7,7 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
 const Hello = props => (
-  <div>Hello {props.name}!</div>
+  <h1>Hello {props.name}!</h1>
 )
 
 Hello.defaultProps = {
